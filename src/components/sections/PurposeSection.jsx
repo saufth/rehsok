26d6dade870cc51f9c2e68890c0fa35eebaf0ac4
Saufth <saufth@gmail.com>
2022-11-07 07:14:52 -0600

    SpaceTopXS on Spacing styles reduced

--- src/components/sections/PurposeSection.jsx
+++ src/components/sections/PurposeSection.jsx
@@ -31,7 +31,7 @@ const PurposeSection = () => {
           </Heading>
         </div>
 
-        <Paragraph center small space='top-xs'>
+        <Paragraph center space='top-xs'>
           Creamos posibilidades para empresas orientadas al progreso, 
           con el objetivo de construir un legado de verdad.
         </Paragraph>
