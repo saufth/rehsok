74271e06084eab5f919e38479e68568829902bff
Saufth <saufth@gmail.com>
2022-11-14 13:59:02 -0600

    env config added to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_EMAIL_SERVICE_KEY: process.env.NEXT_PUBLIC_EMAIL_SERVICE_KEY,
+    NEXT_PUBLIC_EMAIL_TEMPLATE_KEY: process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_KEY,
+    NEXT_PUBLIC_EMAIL_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY
+  },
   experimental: {
     appDir: true
   }
