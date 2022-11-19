81f32fbc500f267ea057518f1fd196e0f5a8d320
Saufth <saufth@gmail.com>
2022-11-18 23:55:46 -0600

    Loading image fixed

--- src/components/feedback/Loading.jsx
+++ src/components/feedback/Loading.jsx
@@ -16,6 +16,7 @@ const Loading = () => {
           width={160}
           height={160}
           quality='100'
+          priority
         />
       </div>
     </div>
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -28,7 +28,6 @@ const Hero = () => {
             width={192}
             height={195}
             quality='100'
-            priority
           />
         </div>
 
@@ -51,7 +50,6 @@ const Hero = () => {
           objectFit='cover'
           objectPosition='center'
           quality='100'
-          priority
         />
       </div>
     </section>
