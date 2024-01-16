896ec71d4e32fba3f8ca57c22f287414a813d842
saufth <saufth@gmail.com>
2024-01-15 19:29:37 -0600

    Add Image component and update page layout in RazonDeSerPage

--- src/app/(lobby)/razon-de-ser/page.tsx
+++ src/app/(lobby)/razon-de-ser/page.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import Image from 'next/image'
 import { history } from '@/config/organization'
 
 export const metadata: Metadata = {
@@ -11,25 +12,29 @@ export default function RazonDeSerPage () {
   return (
     <>
       <div className='w-full h-[68px] lg:h-[84px] fixed top-0 left-0 bg-black z-20' />
-      <section className='mt-spacing-9'>
-        <div className='container'>
-          <div className='border-t'>
-            <div className='cols-container pt-spacing-3'>
-              <div className='w-6-cols md:w-4-cols lg:w-6-cols'>
-                <h1 className='f-heading-1 font-serif md:pr-9 lg:pr-11'>
-                  {history.title}
-                </h1>
-              </div>
-              {history.description && (
-                <div className='w-6-cols md:w-4-cols lg:w-6-cols mt-4 md:mt-0'>
-                  <p className='f-subhead-1'>
-                    {history.description}
-                  </p>
-                </div>
-              )}
+      <section>
+        <div className='container pt-spacing-7'>
+          <div className='cols-container pt-spacing-3'>
+            <div className='w-6-cols md:w-4-cols lg:w-6-cols'>
+              <h1 className='f-display-2 md:pr-9 lg:pr-11 font-serif'>
+                Razón de ser
+              </h1>
             </div>
           </div>
-          <div className='mt-spacing-6'>
+        </div>
+        <div className='full-bleed-container mt-spacing-7'>
+          <Image
+            src='/images/cultura-hero.webp'
+            alt='Un joven y feliz hombre de negocios está escribiendo en una computadora portátil mientras trabaja remotamente desde la cafetería'
+            width={3200}
+            height={2400}
+            sizes='(max-width: 744px) 100vw, (max-width: 1280px) 100vw, (max-width: 1440px) 100vw, 100vw'
+            loading='lazy'
+            className='w-full'
+          />
+        </div>
+        <div className='container mt-spacing-7'>
+          <div className='border-t mt-spacing-3'>
             {history.items.map((historyItem, key) => (
               <p className='f-subhead-2 mt-spacing-3' key={key}>
                 {historyItem}
